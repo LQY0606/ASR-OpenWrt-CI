c10592cbf6815c3e2d28f8434623d1bc7fe53083
LQY0606 <91514324+LQY0606@users.noreply.github.com>
2023-10-20 21:15:14 +0800

    Update Plugins.sh

--- Scripts/Plugins.sh
+++ Scripts/Plugins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Design Theme
-git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "main" || echo "js") https://github.com/papagaye744/luci-theme-design.git
-git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
+#git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "main" || echo "js") https://github.com/papagaye744/luci-theme-design.git
+#git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-config.git
 #Argon Theme
 #git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-theme-argon.git
 #git clone --depth=1 --single-branch --branch $(echo $OpenWrt_URL | grep -iEq "lede|padavanonly" && echo "18.06" || echo "master") https://github.com/jerrykuku/luci-app-argon-config.git
@@ -17,4 +17,4 @@ git clone --depth=1 --single-branch https://github.com/gngpp/luci-app-design-con
 #git clone --depth=1 --single-branch --branch "master" https://github.com/sbwml/luci-app-alist.git
 
 #lucky
-git clone --depth=1 --single-branch --branch "main" https://github.com/gdy666/luci-app-lucky.git
+#git clone --depth=1 --single-branch --branch "main" https://github.com/gdy666/luci-app-lucky.git
